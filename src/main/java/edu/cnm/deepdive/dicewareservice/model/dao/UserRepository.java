3f35150593ba66fa0659a17f7ee8db2c1b777086
Andrew Patten <andpatten@gmail.com>
2019-11-04 10:11:24 -0700

    Added User table and user principle.

--- src/main/java/edu/cnm/deepdive/dicewareservice/model/dao/UserRepository.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/model/dao/UserRepository.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.dicewareservice.model.dao;
 
 import edu.cnm.deepdive.dicewareservice.model.entity.User;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
